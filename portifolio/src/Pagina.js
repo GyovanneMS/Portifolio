074a3e93a53c7408761e5bc7d1d943aaa07d024b
GyovanneMS <gyovannecontato@gmail.com>
2024-03-08 15:39:12 -0300

    making my portifolio

---
+++ portifolio/src/Pagina.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Pagina(){
+    return <main>
+        <img src="./chidi.jpg" alt="foto" width="128" />
+        <div>
+            <b> Gyovanne Martins Sabará</b> | 222 meses | Desenvolvedor Web
+        </div>
+
+        <div>
+            <b> Perfil </b> | 222 meses | Desenvolvedor Web
+        </div>
+        
+        Programador: Desvendando Códigos, Construindo o Futuro
+
+        Sou um programador apaixonado por novas esperiências, com vontade de aprender mais e mais e ajudar na empresa que eu entrar, com um talento nato para construir soluções inovadoras.
+
+        Minha mente analítica e meu entusiasmo me impulsionam a buscar aperfeiçoar minhas habilidades constantemente.
+
+        Possuo um amplo conhecimento em diversas linguagens de programação e ferramentas, incluindo:
+    </main>
+}
