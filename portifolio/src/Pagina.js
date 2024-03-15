e68a440a5e0dce0ba911e54d474b25740bddf252
GyovanneMS <gyovannecontato@gmail.com>
2024-03-15 15:28:26 -0300

    Some divs and Sbetter tyle

--- portifolio/src/Pagina.js
+++ portifolio/src/Pagina.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Pagina(){
+export default function Pagina() {
     return <main>
         <img src="./chidi.jpg" alt="foto" width="128" />
         <div>
@@ -8,15 +8,42 @@ export default function Pagina(){
         </div>
 
         <div>
-            <b> Perfil </b> | 222 meses | Desenvolvedor Web
+            <b> Perfil </b>
+            Programador: Desvendando Códigos, Construindo o Futuro
+
+            Sou um programador apaixonado por novas esperiências, com vontade de aprender mais e mais e ajudar na empresa que eu entrar, com um talento nato para construir soluções inovadoras.
+
+            Minha mente analítica e meu entusiasmo me impulsionam a buscar aperfeiçoar minhas habilidades constantemente.
+
+            Possuo um amplo conhecimento em diversas linguagens de programação e ferramentas, incluindo:
         </div>
-        
-        Programador: Desvendando Códigos, Construindo o Futuro
 
-        Sou um programador apaixonado por novas esperiências, com vontade de aprender mais e mais e ajudar na empresa que eu entrar, com um talento nato para construir soluções inovadoras.
-
-        Minha mente analítica e meu entusiasmo me impulsionam a buscar aperfeiçoar minhas habilidades constantemente.
-
-        Possuo um amplo conhecimento em diversas linguagens de programação e ferramentas, incluindo:
+        <div>
+            <b> Experiências</b>: Já trabalhei com XXXXX, desenvolvendo projetos como XXXXXXXX e passei 00 anos fazendo curso na Supergeeks.
+        </div>
+        <div>
+            <b> Competências</b>:
+            Lógica de programação;
+            Raciocínio Computacional,
+            Python,
+            C#,
+            HTML,
+            CSS,
+            JavaScript,
+            React,
+            React Native,
+            Ruby on Rails,
+            Cobol,
+            Java,
+            Mongoo,
+            SQL,
+            z/OS e
+            Pacote Office.
+        </div>
+        <div>
+            <a href="gy.martinssabara@gmail.com"> Email </a>
+            <a href="tel: +55 (11) 97817-9999"> Celular </a>
+            <a href="https://github.com/GyovanneMS"> Github </a>
+        </div>
     </main>
 }
